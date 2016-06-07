a2daf9d867fe8f5bbf620c54c3c4184525c14726
franksantos <frank_81santos@hotmail.com>
2016-06-07 18:55:54 -0300

    Alterado a migration CreateTriggerInsPagto para adequar ao PostgreSql

--- database/migrations/2016_02_18_195944_CreateTriggerInsPagto.php
+++ database/migrations/2016_02_18_195944_CreateTriggerInsPagto.php
@@ -13,14 +13,25 @@ class CreateTriggerInsPagto extends Migration
     public function up()
     {
         //
-        DB::unprepared('
+        /*DB::unprepared('
         CREATE TRIGGER tr_Update_Mensalidade_Apos_Pagamento AFTER INSERT ON `pagamento` FOR EACH ROW
         BEGIN
          UPDATE mensalidade SET mes_status="Pago" WHERE id=NEW.pag_mes_id;
         END
-        ');
-    }
+        ');*/
+        DB::unprepared("
+        CREATE OR REPLACE FUNCTION process_mensalidade_pagamento() RETURNS TRIGGER AS \$trg_mes_pagto\$
+            BEGIN
+                UPDATE mensalidade SET mes_status='Pago' WHERE id=NEW.pag_mes_id;
+                RETURN NULL; -- result is ignored since this is an AFTER trigger
+            END;
+        \$trg_mes_pagto\$ LANGUAGE plpgsql;
 
+        CREATE TRIGGER trg_mes_pagto
+        AFTER INSERT ON pagamento
+            FOR EACH ROW EXECUTE PROCEDURE process_mensalidade_pagamento();
+            ");
+    }
     /**
      * Reverse the migrations.
      *
