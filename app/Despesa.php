4f15d16aed5204f9e16aa2fdec6c1f52f2259ed1
frank <newtonrep@gmail.com>
2018-09-10 20:57:16 -0300

    criado model despesa

--- app/Despesa.php
+++ app/Despesa.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Despesa extends Model
 {
     //
+    /** situacao indica a situação da despesa se está     */
+    /** 0 = Em Aberto     */
+    /** 1 = Paga     */
+    /** 2 = Atrasada     */
+    /** tipo_pagamento indica o tipo de pagamento da despesa    */
+    /** 0 = Dinheiro     */
+    /** 1 = Cartão     */
+    /** 2 = Paypal     */
     protected $fillable = [
       'descricao',
       'vencimento',
