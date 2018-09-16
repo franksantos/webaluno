c323ca71c7385f8d338549b5429019b4722996a7
frank <newtonrep@gmail.com>
2018-09-16 17:52:55 -0300

    economizando espaço na view

--- resources/views/despesa/create.blade.php
+++ resources/views/despesa/create.blade.php
@@ -30,7 +30,7 @@
         <!-- aqui entrara a tela de detalhes do pagamento -->
         {!! Form::open(array('url' => 'despesa/store')) !!}
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-3">
                 <div class="form-group">
                     {!! Form::label('Descrição') !!}
                     {!! Form::text('descricao', null, ['class'=>'form-control']) !!}
@@ -40,6 +40,8 @@
                     {!! Form::text('vencimento', null, ['id'=>'vencimento','class'=>'form-control']) !!}
 
                 </div>
+            </div>
+            <div class="col-md-3">
                 <div class="form-group">
                     {!! Form::label('Tipo de Pagamento') !!}
                     {!! Form::radio('tipo_pagamento', "0", ['class'=>'form-control']) !!} Dinheiro
@@ -52,7 +54,7 @@
                     {!! Form::radio('situacao', '2', ['class'=>'form-control text-red']) !!} Em Atraso
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-3">
                 <div class="form-group">
                     {!! Form::label('Valor ( em Reais )') !!}
                     {!! Form::number('valor', null, [
@@ -64,65 +66,77 @@
                 </div>
                 <div class="form-group">
                     {!! Form::label('Data do Pagamento') !!}
-                    {!! Form::text('data_pagamento', null, ['id'=>'data_pagamento', 'class'=>'form-control']) !!}
-                </div>
-                <div class="form-group">
-                    {!! Form::label('Observação') !!}
-                    {!! Form::textarea('obs',null, ['id'=>'valor_parcela','class' => 'form-control']) !!}
-                </div>
-                <div class="form-group">
-                    {!! Form::submit('Salvar', ['class' => 'btn btn-primary']) !!}
+                    {!! Form::text('data_pagamento', null, ['id'=>'data_pagamento', 'class'=>'form-control', 'autocomplete'=>'off']) !!}
                 </div>
             </div>
-            {{-- <div class="row">
-                <div class="col-sm-4">
-                    <a href="javascript:history.go(-1)" class="btn btn-success">Voltar</a>
-                </div>
-            </div> --}}
         </div>
         <div class="row">
           <div class="col-md-12">
-            <div class="container">
-                    <h4>Despesas</h4>
-                <table class="table table-striped table-bordered table-hover">
-                    <thead>
-                    <tr>
-                        <th>Descrição</th>
-                        <th>Valor</th>
-                        <th>Data de Vencimento</th>
-                        <th>Status</th>
-                        <th style="text-align: center">A&ccedil;&otilde;es</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($despesas as $d)
-                        <tr>
-                            <td>{{ $d->descricao }}</td>
-                            <td>{{ $d->valor }}</td>
-                            <td>{{ $d->vencimento->format('d/m/Y') }}</td>
-                            <td>
-                                @if($d->situacao == 1)
-                                    <p class="text-success"><strong>Pago</strong></p>
-                                @elseif($d->situacao == 0)
-                                    <p class="text-danger"><strong>Em aberto</strong></p>
-                                @endif
-                                {{-- $mensalidade->mes_status --}}
-                            </td>
-                            <td style="text-align: center">
-                                {{-- @if($mensalidade->mes_status == 'Pago')
-                                    <a href="{{route('pagamentos.show', ['id'=>$mensalidade->id])}}" class="btn btn-info">Ver Detalhes do Pagto.</a>
-                                @else
-                                    <a href="{{ route('pagamentos.cadcomprovante',['idMensalidade'=>$mensalidade->id, 'idAluno'=>$alunos->id]) }}" class="btn-sm btn-success">Confirmar Pagamento</a>
-                                @endif --}}
-                                
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+            <div class="form-group">
+                {!! Form::label('Observação') !!}
+                {!! Form::textarea('obs',null, ['id'=>'valor_parcela','class' => 'form-control']) !!}
             </div>
           </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
 
+          </div>
+          <div class="col-md-4">
+            <div class="form-group">
+                {!! Form::submit('Salvar', ['class' => 'btn btn-primary']) !!}
+            </div>
+          </div>
+          <div class="col-md-4">
+
+          </div>
+        </div>
+
+            {{-- Linha da tabela --}}
+            <div class="row">
+              <div class="col-md-12">
+                <div class="container">
+                        <h4>Despesas</h4>
+                    <table class="table table-striped table-bordered table-hover">
+                        <thead>
+                        <tr>
+                            <th>Descrição</th>
+                            <th>Valor</th>
+                            <th>Data de Vencimento</th>
+                            <th>Status</th>
+                            <th style="text-align: center">A&ccedil;&otilde;es</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($despesas as $d)
+                            <tr>
+                                <td>{{ $d->descricao }}</td>
+                                <td>{{ $d->valor }}</td>
+                                <td>{{ $d->vencimento->format('d/m/Y') }}</td>
+                                <td>
+                                    @if($d->situacao == 1)
+                                        <p class="text-success"><strong>Pago</strong></p>
+                                    @elseif($d->situacao == 0)
+                                        <p class="text-danger"><strong>Em aberto</strong></p>
+                                    @endif
+                                    {{-- $mensalidade->mes_status --}}
+                                </td>
+                                <td style="text-align: center">
+                                    {{-- @if($mensalidade->mes_status == 'Pago')
+                                        <a href="{{route('pagamentos.show', ['id'=>$mensalidade->id])}}" class="btn btn-info">Ver Detalhes do Pagto.</a>
+                                    @else
+                                        <a href="{{ route('pagamentos.cadcomprovante',['idMensalidade'=>$mensalidade->id, 'idAluno'=>$alunos->id]) }}" class="btn-sm btn-success">Confirmar Pagamento</a>
+                                    @endif --}}
+
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+              </div>
+
+            </div>
         </div>
     </div>
 @endsection
@@ -131,6 +145,13 @@
       $(document).ready(function(){
           //datepicker da data de pagamento
           $('#vencimento').datepicker({
+              format:'dd/mm/yyyy',
+              language: 'pt-BR',
+              autoclose: true,
+              todayHighlight: true
+          });
+          //datepicker da data de pagamento
+          $('#data_pagamento').datepicker({
               format:'dd/mm/yyyy',
               language: 'pt-BR',
               autoclose: true,
